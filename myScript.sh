039855fc5322fdc677e67db87db47f07f22e642d
Daham-Mustaf <s7damust@uni-bonn.de>
2022-07-16 16:48:45 +0200

    COMPARISON

--- myScript.sh
+++ myScript.sh
@@ -17,7 +17,22 @@ NAME="myScript"
 
 
 #If statement
-if [ "$NAME" == "myScript" ]
+# if [ "$NAME" == "myScript" ]
+# then
+#     echo "Your name is myScript"
+# fi
+# if [ "$NAME" == "Java" ]
+# then
+#     echo "Your name is myScript"
+# else
+#     echo "Your name is not myScript"
+# fi
+# COMPARISON
+N1="Java"
+N2="C++"
+if [ "$N1" -gt "$N2" ]
 then
-    echo "Your name is myScript"
+  echo "$N1 is greater than $N2"
+else
+  echo "$N1 is less than $N2"
 fi
